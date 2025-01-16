025de3205fb51977ceb38cdc7ec947d76b77d5bf
murilo <murilocarrijoadm@gmail.com>
2025-01-16 09:22:31 -0300

    The encode methos has been created

--- encode_list.rb
+++ encode_list.rb
@@ -3,4 +3,11 @@ class EncodeList
     @list = [1, 2, 3, 4]
     @encoded_list = []
   end
+
+  def encode
+    @list.shuffle.each do |item|
+      @encoded_list << item
+    end
+    @encoded_list
+  end
 end
